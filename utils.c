4ce6fbea3205580c0d7092ef1f56e620c4ca46df
Alex Machine <meklex1@gmail.com>
2022-10-17 18:54:16 +0100

    fixed some typos

--- utils.c
+++ utils.c
@@ -13,7 +13,7 @@ int is_printable(char c)
 }
 
 /**
- * append_hexa_code - Append ascci in hexadecimal code to buffer
+ * append_hexa_code - Append ascci in hexadecimal code to buffe
  * @buffer: Array of chars.
  * @i: Index at which to start appending.
  * @ascii_code: ASSCI CODE.
